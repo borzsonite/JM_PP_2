e098249f311aea992022319d7db4bdb316f4e4bb
Sergey <borzsonite@gmail.com>
2021-06-18 23:36:34 +0300

    2.1.2 done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(catBean2.getGreetingMessage());
+        System.out.println(catBean == catBean2);
+
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    private String greetingMessage;
+
+    public Cat() {
+    }
+
+    public String getGreetingMessage() {
+        return greetingMessage;
+    }
+
+    public void setGreetingMessage(String greetingMessage) {
+        this.greetingMessage = greetingMessage;
+    }
+}
